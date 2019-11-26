cbdc5aa8a47abdb693b9765730e5c89813b749c0
bruinen <max.machnik@gmail.com>
2019-11-26 22:28:28 +0100

    A to Z with animation, dots for mobile

--- js/productsCount.js
+++ js/productsCount.js
@@ -9,11 +9,21 @@ const wagon = document.querySelector('.yearsOnMarket__wagon');
 const yearsText = document.querySelector('.yearsOnMarket__text');
 const train = document.querySelector('.yearsOnMarket__train');
 
+const bigLettersCont = document.querySelector('.bigLetters');
+const bigLettersText = document.querySelector('.bigLetters__arrowText');
+const bigA = document.querySelector('.bigLetters__letter--a');
+const bigZ = document.querySelector('.bigLetters__letter--z');
+const timeFrom = document.querySelector('.bigLetters__time--from');
+const timeTo = document.querySelector('.bigLetters__time--to');
+
 const getElScroll = (el, startOnTop) => {
 	const windowHeight = window.innerHeight;
 	const elRec = el.getBoundingClientRect();
 	const elPos = elRec.top + elRec.height * (startOnTop ? 0 : 1 / 2);
-	if (elPos < windowHeight + elRec.height && elRec.bottom > elRec.height / 6) {
+	if (
+		elPos < windowHeight + elRec.height &&
+		elRec.bottom > elRec.height / 6
+	) {
 		return (200 * (elPos - windowHeight / 2)) / windowHeight;
 	}
 };
@@ -29,9 +39,20 @@ const animateCounter = () => {
 		eventsCount.style.transform = `translate(${-0.5 *
 			Math.abs(getElScroll(eventsCount))}%, ${-0.5 *
 			Math.abs(getElScroll(eventsCount))}%)`;
-    }
-    
-    // Years on market trolley problem
+	}
+
+	// A to Z
+	let bigLTPace = window.innerWidth < 960 ? 0.1 : 0.175;
+	bigLettersText.style.transform = `translateY(${-bigLTPace *
+		getElScroll(bigLettersCont)}vh) rotate(-90deg)`;
+	const AOpacity = (0.5 * getElScroll(bigLettersCont) + 20);
+	const ZOpacity = -1*AOpacity + 40;
+	bigA.style.opacity = AOpacity+'%';
+	timeFrom.style.opacity = AOpacity+'%';
+	bigZ.style.opacity = ZOpacity+'%';
+	timeTo.style.opacity = ZOpacity+'%';
+
+	// Years on market trolley problem
 	if (window.innerWidth < 960) {
 		wagon.style.transform = `translate(${-0.346 *
 			getElScroll(wagon, true)}vh, ${-0.2 * getElScroll(wagon, true)}vh)`;
@@ -39,12 +60,12 @@ const animateCounter = () => {
 			getElScroll(train)}vh, ${0.2 *
 			getElScroll(train)}vh) rotate(30deg)`;
 	} else {
-        wagon.style.transform = `translate(${-0.692 *
+		wagon.style.transform = `translate(${-0.692 *
 			getElScroll(wagon, true)}vh, ${-0.4 * getElScroll(wagon, true)}vh)`;
 		yearsText.style.transform = `translate(${0.692 *
 			getElScroll(train)}vh, ${0.4 *
 			getElScroll(train)}vh) rotate(30deg)`;
-    }
+	}
 };
 
 window.addEventListener('scroll', animateCounter);
