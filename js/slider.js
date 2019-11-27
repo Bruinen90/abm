c1918b3b223456aa796dfdab63bd38c5ad4a1173
bruinen <max.machnik@gmail.com>
2019-11-27 23:25:01 +0100

    Gallery lock on max transform, pure css arrows on desktop 50%

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,28 @@
 const arrowPrev = document.querySelector('.slider__arrow--prev');
 const arrowNext = document.querySelector('.slider__arrow--next');
-
 const photosCont = document.querySelector('.slider__imagesCont');
 
 const photosCount = photosCont.querySelectorAll('img').length;
 
 let currentPhoto = 0;
+const firstImgWidth = photosCont
+	.querySelectorAll('img')[0]
+	.getBoundingClientRect().width;
+const picPerScreen = window.innerWidth / firstImgWidth;
 
-const changePhoto = (direction) => {
-    const currImgWidth = photosCont.querySelectorAll('img')[0].getBoundingClientRect().width;
-    console.log(currImgWidth)
-    if(currentPhoto+direction > -1 && currentPhoto+direction < photosCount) {
-        currentPhoto += direction;
-        photosCont.style.transform = `translateX(${-currImgWidth*currentPhoto}px)`
-        console.log('changing photo')
-    }
-}
+const changePhoto = direction => {
+	if (currentPhoto + direction > -1) {
+		currentPhoto += direction;
+		let transformInPx = firstImgWidth * currentPhoto;
+		const maxTransform =
+			firstImgWidth * photosCount - picPerScreen * firstImgWidth;
+		if (transformInPx > maxTransform) {
+            transformInPx = maxTransform;
+            currentPhoto -= direction;
+		}
+		photosCont.style.transform = `translateX(${-transformInPx}px)`;
+	}
+};
 
-arrowPrev.addEventListener('click', ()=>changePhoto(-1));
-arrowNext.addEventListener('click', ()=>changePhoto(1));
+arrowPrev.addEventListener('click', () => changePhoto(-1));
+arrowNext.addEventListener('click', () => changePhoto(1));
