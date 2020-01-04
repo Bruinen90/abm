7520ce0455e133d518404942b8863b7ebcf93f6a
bruinen <max.machnik@gmail.com>
2020-01-04 19:59:41 +0100

    75% of free changes done

--- js/welcome.js
+++ js/welcome.js
@@ -1,10 +1,9 @@
 const wrapper = document.querySelector('.welcome__button--desktop');
 const cellsArr = document.querySelectorAll('.welcome__text--number');
 
-
 cellsArr.forEach(cell => {
-    cell.addEventListener('mouseover', ()=>{
-        wrapper.innerHTML = `<span class="welcome__bordered">0${cell.dataset.number}. </span>
-        ${cell.dataset.text}`
-    })
-})
+	cell.addEventListener('mouseover', () => {
+		wrapper.innerHTML = `<span class="welcome__bordered">0${cell.dataset.number}. </span>
+        ${cell.dataset.text}`;
+	});
+});
